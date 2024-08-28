7fb1e3323c8e4fe03fe0b2158ba3fefb29cda668
Al-Azeem Abdul Rasaq <93706390+AlAzeemRasaq@users.noreply.github.com>
2024-08-28 18:48:23 +0100

    removed 'protected' and 'virtual'

--- HopperBug.h
+++ HopperBug.h
@@ -8,14 +8,12 @@
 using namespace std;
 
 class HopperBug: public Bug {
-protected:
-
     int hopLength; // how much the bug will jump
 private:
     int x, y;
 public:
     HopperBug(string type, int id, std::pair<int, int> position, Direction direction, int size, int hopLength);
-    virtual void move();
+    void move();
 };
 
 #endif // CLASSES_HOPPERBUG_H
